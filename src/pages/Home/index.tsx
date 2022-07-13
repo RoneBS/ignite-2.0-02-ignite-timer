297deb93db9bd4cf047b0f97536b6b4f5f204379
Rone Silveira <ronebs10@hotmail.com>
2022-07-13 08:48:49 -0300

    Home page layout complete

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,51 @@
+import { Play } from 'phosphor-react'
+import * as S from './styles'
+
+export const Home = () => {
+  return (
+    <S.HomeContainer>
+      <form>
+        <S.FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <S.TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Porjeto 1" />
+            <option value="Porjeto 2" />
+            <option value="Porjeto 3" />
+            <option value="banana" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <S.MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutos.</span>
+        </S.FormContainer>
+
+        <S.CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <S.Separator>:</S.Separator>
+          <span>0</span>
+          <span>0</span>
+        </S.CountdownContainer>
+
+        <S.StartCountdownButton disabled type="submit">
+          <Play size={24} />
+          Começar
+        </S.StartCountdownButton>
+      </form>
+    </S.HomeContainer>
+  )
+}
